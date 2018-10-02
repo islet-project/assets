9af15ea3e11a19e5df91de04b4c5e99023718e20
Sandrine Bailleux <sandrine.bailleux@arm.com>
2018-10-02 15:17:32 +0200

    Add meta tests makefiles
    Introduce makefiles for 2 popular sets of tests: the standard tests
    and the extensive ones.
    
    Change-Id: I529a1583a1b20838c60f06654d3f47f4041fb9bc
    Signed-off-by: Sandrine Bailleux <sandrine.bailleux@arm.com>

---
+++ tftf/tests/tests-common.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2018, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+TESTS_MAKEFILE := $(addprefix tftf/tests/,	\
+	tests-arm-state-switch.mk		\
+	tests-boot-req.mk			\
+	tests-cpu-extensions.mk			\
+	tests-el3-power-state.mk		\
+	tests-fwu.mk				\
+	tests-manual.mk				\
+	tests-performance.mk			\
+	tests-psci.mk				\
+	tests-runtime-instrumentation.mk	\
+	tests-sdei.mk				\
+	tests-single-fault.mk			\
+	tests-spm.mk				\
+	tests-template.mk			\
+	tests-tftf-validation.mk		\
+	tests-tsp.mk				\
+	tests-uncontainable.mk			\
+)
+
+include ${TESTS_MAKEFILE}
---
+++ tftf/tests/tests-extensive.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2018, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# Run all standard tests, plus the extensive ones.
+include tftf/tests/tests-common.mk
+TESTS_MAKEFILE += tftf/tests/tests-psci-extensive.mk
+
+include ${TESTS_MAKEFILE}
