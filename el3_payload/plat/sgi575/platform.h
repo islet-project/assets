2a287fc4ad5fc0e3b0a5e798efc12ab572863958
Chandni Cherukuri <chandni.cherukuri@arm.com>
2018-11-13 16:18:46 +0530

    el3_payload: Add initial platform support for SGI-575
    SGI-575 platform consists of two clusters of four Cortex-A75 CPUs
    each. CPU0 of the first cluster is the primary CPU. SoC UART0 is
    used for the console log.
    
    Change-Id: I9e3c188643aa699f57bc3f51e9d15f44f84f987c
    Signed-off-by: Chandni Cherukuri <chandni.cherukuri@arm.com>

---
+++ el3_payload/plat/sgi575/platform.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+#define PRIMARY_CPU_MPID		0x0
+
+#define CPUS_COUNT			8
+
+#define UART_BASE			0x7ff80000
+
+#define SGI_MAX_CPUS_PER_CLUSTER        4
+#define SGI_MAX_PE_PER_CPU              1
+
+#endif /* PLATFORM_H */
---
+++ el3_payload/plat/sgi575/platform.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2018, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+DRAM_BASE	:=	0x80000000
+DRAM_SIZE	:=	0x80000000
