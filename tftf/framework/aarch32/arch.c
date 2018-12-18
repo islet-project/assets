dddec6bc4834e4e83c7d6d3a24abcc4dd6b631e5
Sandrine Bailleux <sandrine.bailleux@arm.com>
2018-12-18 15:45:01 +0100

    Explain why we set HCR.TGE bit
    Change-Id: Id55a62e8610f0b162a26a0eeb8f69e4a87910ed7
    Signed-off-by: Sandrine Bailleux <sandrine.bailleux@arm.com>

--- tftf/framework/aarch32/arch.c
+++ tftf/framework/aarch32/arch.c
@@ -14,5 +14,10 @@ void tftf_arch_setup(void)
 	if (!IS_IN_HYP())
 		panic();
 
+	/*
+	 * Route physical interrupts to Hyp mode regardless of the value of the
+	 * IMO/FMO bits. Without this, interrupts would not be taken and would
+	 * remain pending, regardless of the PSTATE.{A, I, F} interrupt masks.
+	 */
 	write_hcr(HCR_TGE_BIT);
 }
--- tftf/framework/aarch64/arch.c
+++ tftf/framework/aarch64/arch.c
@@ -10,6 +10,12 @@ void tftf_arch_setup(void)
 {
 	/* Do not try to configure EL2 if TFTF is running at NS-EL1 */
 	if (IS_IN_EL2()) {
+		/*
+		 * Route physical interrupts to EL2 regardless of the value of
+		 * the IMO/FMO bits. Without this, interrupts would not be taken
+		 * and would remain pending, regardless of the PSTATE.{A, I, F}
+		 * interrupt masks.
+		 */
 		write_hcr_el2(HCR_TGE_BIT);
 		isb();
 	}
