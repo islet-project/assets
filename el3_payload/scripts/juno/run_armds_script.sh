47ab4f4fd1c650eff22844593577814a095952dd
Sandrine Bailleux <sandrine.bailleux@arm.com>
2019-05-10 13:23:41 +0200

    Adapt run_ds5_script.sh for recent Arm DS versions
    Arm DS-5 has been superseded by Arm Development Studio. Do the necessary
    changes to the EL3 payload scripts to migrate to the latter:
    
     - Rename run_ds5_script.sh into run_armds_script.sh.
     - The command-line debugger is now called armdbg.
     - The CDB entry string for Arm Juno development board has changed.
    
    Change-Id: Idc640e7fcff790d5a1bf6e938e14a0dbc88029b9
    Signed-off-by: Sandrine Bailleux <sandrine.bailleux@arm.com>

--- el3_payload/scripts/juno/run_armds_script.sh
+++ el3_payload/scripts/juno/run_armds_script.sh
@@ -5,26 +5,26 @@ set -e
 # Expect the script to run in argument
 if [ $# != 1 ]; then
 	echo "ERROR: No script provided"
-	echo "usage: $(basename $0) <ds5_script_to_run>"
+	echo "usage: $(basename $0) <armds_script_to_run>"
 	exit 1
 fi
 
 # Is DS-5 command-line debugger found?
-if [ ! $(which debugger) ]; then
+if [ ! $(which armdbg) ]; then
 	echo 'ERROR: Failed to find DS-5 command-line debugger.'
-	echo 'Please add the path to the command-line debugger in your PATH.'
+	echo 'Please add the path to the armdbg program in your PATH.'
 	echo 'E.g.: export PATH=<DS-5 install dir>/bin:$PATH'
 	exit 1
 fi
 
 # DS-5 configuration database entry for Juno r0
-juno_cdb_entry='ARM Development Boards::Juno ARM Development Platform (r0)::Bare Metal Debug::Bare Metal Debug::Debug Cortex-A53_0::DSTREAM'
+juno_cdb_entry='Arm::Juno Arm Development Platform (r0)::Bare Metal Debug::Bare Metal Debug::Debug Cortex-A53_0::DSTREAM'
 
 # Browse for available DSTREAM connections and lists targets that match the
 # connection type specified in the configuration database entry
 echo "Trying to detect your DSTREAM unit..."
 connections_list=available_connections
-debugger --cdb-entry "$juno_cdb_entry" --browse \
+armdbg --cdb-entry "$juno_cdb_entry" --browse \
   | tee $connections_list
 
 # Remove first line in the file (i.e. "Available connections:")
@@ -42,7 +42,7 @@ fi
 
 # Run DS-5 script
 echo "Connecting to $connection..."
-debugger \
+armdbg \
 	--cdb-entry "$juno_cdb_entry" \
 	--cdb-entry-param "Connection=$connection" \
 	--stop_on_connect=false \
