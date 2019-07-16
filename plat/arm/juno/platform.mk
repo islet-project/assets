d1193ef6d09b94e5c5083b868371d1ace9149854
John Tsichritzis <john.tsichritzis@arm.com>
2019-07-16 15:30:22 +0100

    Skip multicore spurious interrupt test
    This specific test is known to have unstable behaviour. As a temporary
    solution we skip this test for AArch64 Juno configs.
    
    Change-Id: I7c93de0c80ad8dba47b83f0b343d868dc18b732f
    Signed-off-by: John Tsichritzis <john.tsichritzis@arm.com>

--- plat/arm/juno/platform.mk
+++ plat/arm/juno/platform.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2018, Arm Limited. All rights reserved.
+# Copyright (c) 2018-2019, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -18,9 +18,11 @@ PLAT_SOURCES	:=	drivers/arm/gic/arm_gic_v2.c			\
 			plat/arm/juno/juno_mem_prot.c			\
 			plat/arm/juno/plat_setup.c
 
-# Some tests are not supported on Juno AArch32.
 ifeq (${ARCH},aarch32)
-PLAT_TESTS_SKIP_LIST	:=	plat/arm/juno/juno32_tests_to_skip.txt
+# Some tests are not supported on Juno AArch32.
+	PLAT_TESTS_SKIP_LIST	:=	plat/arm/juno/juno32_tests_to_skip.txt
+else
+	PLAT_TESTS_SKIP_LIST	:=	plat/arm/juno/juno64_tests_to_skip.txt
 endif
 
 PLAT_SUPPORTS_NS_RESET	:=	1
