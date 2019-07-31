89391603d63fd409c18e00e45c864dc9749f2f25
laurenw-arm <lauren.wehrmeister@arm.com>
2019-07-31 15:33:07 -0500

    Create new xml to split Juno reboot tests
    Creating a new xml to separate Juno TFTF reboot tests by those that are expected to pass and
    those that aren't (2 RESET2 tests).
    
    Signed-off-by: Lauren Wehrmeister <lauren.wehrmeister@arm.com>
    Change-Id: I101de828aeedd33e0601597b70e00056b884ceba

---
+++ tftf/tests/tests-reboot_reset2.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2019, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+TESTS_SOURCES	+=							\
+	$(addprefix tftf/tests/runtime_services/standard_service/psci/api_tests/, \
+		mem_protect/test_mem_protect.c				\
+		psci_stat/test_psci_stat.c				\
+		reset2/reset2.c 					\
+	)
+
+ifeq (${USE_NVM},0)
+$(error Reboot tests require USE_NVM=1 to persist test results across reboots)
+endif
+
+ifeq (${NEW_TEST_SESSION},1)
+$(error Reboot tests require NEW_TEST_SESSION=0 to persist test results across reboots)
+endif
