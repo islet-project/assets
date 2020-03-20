35106bd23a43797cd3ea74994498dc6cc1ceb812
Varun Wadekar <vwadekar@nvidia.com>
2020-03-20 10:12:03 -0700

    Tegra194: introduce system reset
    This patch introduces the flexibility to reset the platform via
    the SMC_PSCI_SYSTEM_RESET function ID.
    
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    Change-Id: I315315f1998d9b9d8f237ac05625328175880922

--- plat/nvidia/tegra194/platform.mk
+++ plat/nvidia/tegra194/platform.mk
@@ -14,6 +14,7 @@ PLAT_SOURCES		:=	drivers/arm/gic/arm_gic_v2.c			\
 				plat/nvidia/tegra194/helpers.S			\
 				plat/nvidia/tegra194/pwr_state.c		\
 				plat/nvidia/tegra194/pwr_mgmt.c			\
+				plat/nvidia/tegra194/reset.c			\
 				plat/nvidia/tegra194/setup.c			\
 				plat/nvidia/tegra194/topology.c			\
 				plat/nvidia/tegra194/timers.c			\
@@ -24,6 +25,12 @@ PLAT_TESTS_SKIP_LIST	:=	plat/nvidia/tegra194/tests_to_skip.txt
 TFTF_CFLAGS		+=	-Wno-maybe-uninitialized
 
 ENABLE_ASSERTIONS	:=	1
+
+PLAT_SUPPORTS_NS_RESET	:=	1
+
+# Process PLAT_SUPPORTS_NS_RESET flag
+$(eval $(call assert_boolean,PLAT_SUPPORTS_NS_RESET))
+$(eval $(call add_define,TFTF_DEFINES,PLAT_SUPPORTS_NS_RESET))
 
 ifeq ($(USE_NVM),1)
 $(error "Tegra194 port of TFTF doesn't currently support USE_NVM=1")
