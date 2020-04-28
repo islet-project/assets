bc93a34c0db8adc674450b3452868ff3a1ec7503
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2020-04-28 17:59:32 +0100

    Add test case for SMCCC_ARCH_SOC_ID feature
    Added test case for "SMCCC_ARCH_SOC_ID" SMC call.
    This SMC call is used to retrieve SOC version and SOC revision
    
    Test execution output is as below:
    
    > Executing 'SMCCC_ARCH_SOC_ID test'
      TEST COMPLETE                                                 Passed
    SOC Rev is not implemented
    SOC Ver = 0x43b0000
    
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>
    Change-Id: I48668ce22bb5d5767dadb42ce9526d77fd916bed

--- tftf/tests/tests-cpu-extensions.mk
+++ tftf/tests/tests-cpu-extensions.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2018-2019, Arm Limited. All rights reserved.
+# Copyright (c) 2018-2020, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -11,5 +11,6 @@ TESTS_SOURCES	+=	$(addprefix tftf/tests/,			\
 	extensions/sve/test_sve.c					\
 	runtime_services/arm_arch_svc/smccc_arch_workaround_1.c		\
 	runtime_services/arm_arch_svc/smccc_arch_workaround_2.c		\
+	runtime_services/arm_arch_svc/smccc_arch_soc_id.c		\
 	extensions/pauth/test_pauth.c					\
 )
