duzailian/tf-a-tests

Tegra: timer: define RTC irq number in each platform
The RTC irq number may be different for different platform.
Introduce new macro TEGRA_PLAT_RTC_IRQ to define the platform
RTC irq number.

Signed-off-by: anzhou <anzhou@nvidia.com>
Change-Id: I5b66b9720fb7fe9a71a5b800764635166da9f1ae

plat/nvidia/tegra186/include/platform_def.h
@@ -156,6 +156,11 @@
 #define TEGRA_SCRATCH_BASE		U(0x0C390000)
 #define TEGRA_SMMU0_BASE		U(0x12000000)
 
+/*******************************************************************************
+ * IRQ value for Tegra Timer0
+ ******************************************************************************/
+#define TEGRA_PLAT_RTC_IRQ		U(42)
+
 #ifndef __ASSEMBLY__
 
 /*

plat/nvidia/tegra194/include/platform_def.h
@@ -163,6 +163,11 @@
  ******************************************************************************/
 #define TEGRA_SMMU_CTX_BASE		(DRAM_END - 0x1000)
 
+/*******************************************************************************
+ * IRQ value for Tegra Timer0
+ ******************************************************************************/
+#define TEGRA_PLAT_RTC_IRQ		U(42)
+
 #ifndef __ASSEMBLY__
 
 /*

islet-project/assets

plat/nvidia/tegra186/include/platform_def.h
@@ -121,6 +121,11 @@
 #define IRQ_PCPU_HP_TIMER		26
 
 /*******************************************************************************
+ * IRQ value for Tegra Timer0
+ ******************************************************************************/
+#define TEGRA_RTC_IRQ			U(42)
+
+/*******************************************************************************
  * Platform specific page table and MMU setup constants
  ******************************************************************************/
 #define PLAT_PHY_ADDR_SPACE_SIZE	(ULL(1) << 40)

plat/nvidia/tegra194/include/platform_def.h
@@ -122,6 +122,11 @@
 #define IRQ_PCPU_HP_TIMER		26
 
 /*******************************************************************************
+ * IRQ value for Tegra Timer0
+ ******************************************************************************/
+#define TEGRA_RTC_IRQ			U(42)
+
+/*******************************************************************************
  * Platform specific page table and MMU setup constants
  ******************************************************************************/
 #define PLAT_PHY_ADDR_SPACE_SIZE	(ULL(1) << 40)