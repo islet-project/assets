duzailian/tf-a-tests

FFA: RXTX map/configure helper macros
Configuring/mapping RXTX buffers results in repetitive code, therefore
created helper macros.

Signed-off-by: J-Alves <joao.alves@arm.com>
Change-Id: I089b072b8b8fc49a445dcfb3967ddd4783b97628

include/runtime_services/ffa_helpers.h
@@ -33,9 +33,30 @@ typedef uint8_t ffa_memory_receiver_flags_t;
 #include <stdint.h>
 
 struct mailbox_buffers {
-	const void *recv;
+	void *recv;
 	void *send;
 };
+
+#define CONFIGURE_MAILBOX(mb_name, buffers_size) 				\
+	do{									\
+	/* Declare RX/TX buffers at virtual FF-A instance */			\
+	static struct {								\
+			uint8_t rx[buffers_size];				\
+			uint8_t tx[buffers_size];				\
+	} __aligned(PAGE_SIZE) mb_buffers;					\
+	mb_name.recv = (void *) mb_buffers.rx;					\
+	mb_name.send = (void *) mb_buffers.tx;					\
+	} while (0)
+
+#define CONFIGURE_AND_MAP_MAILBOX(mb_name, buffers_size, smc_ret)		\
+	do {									\
+	CONFIGURE_MAILBOX(mb_name, buffers_size);				\
+	smc_ret = ffa_rxtx_map(							\
+				(uintptr_t) mb_name.send,			\
+				(uintptr_t) mb_name.recv, 			\
+				buffers_size / PAGE_SIZE			\
+			);							\
+	} while (0)
 
 struct ffa_partition_info {
 	/** The ID of the VM the information is about */

islet-project/assets

include/runtime_services/ffa_helpers.h
@@ -33,9 +33,30 @@ typedef uint8_t ffa_memory_receiver_flags_t;
 #include <stdint.h>
 
 struct mailbox_buffers {
-	const void *recv;
+	void *recv;
 	void *send;
 };
+
+#define CONFIGURE_MAILBOX(mb_name, buffers_size) 				\
+	do {									\
+	/* Declare RX/TX buffers at virtual FF-A instance */			\
+	static struct {								\
+			uint8_t rx[buffers_size];				\
+			uint8_t tx[buffers_size];				\
+	} __aligned(PAGE_SIZE) mb_buffers;					\
+	mb_name.recv = (void *)mb_buffers.rx;					\
+	mb_name.send = (void *)mb_buffers.tx;					\
+	} while (false)
+
+#define CONFIGURE_AND_MAP_MAILBOX(mb_name, buffers_size, smc_ret)		\
+	do {									\
+	CONFIGURE_MAILBOX(mb_name, buffers_size);				\
+	smc_ret = ffa_rxtx_map(							\
+				(uintptr_t)mb_name.send,			\
+				(uintptr_t)mb_name.recv, 			\
+				buffers_size / PAGE_SIZE			\
+			);							\
+	} while (false)
 
 struct ffa_partition_info {
 	/** The ID of the VM the information is about */