duzailian/tf-a-tests

Cactus: decouple exception handling from tftf framework
So far, tftf framework's exception handling was used for Cactus
exceptions also. With new interrupt related tests coming up in
Cactus, we need a separate exception handler code for Cactus.

This patch enables irq/fiq for Cactus and adds placeholder handlers
for them.

Change-Id: Ifd89c4ba8b4491345948bf342540b37fdbc91b8d
Signed-off-by: Manish Pandey <manish.pandey2@arm.com>

spm/cactus/cactus_interrupt.c
@@ -1,0 +1,32 @@
+/*
+ * Copyright (c) 2021, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <debug.h>
+
+#include <ffa_helpers.h>
+#include <sp_helpers.h>
+
+int cactus_irq_handler(void)
+{
+	ffa_int_id_t irq_num;
+
+	irq_num = spm_interrupt_get();
+
+	ERROR("%s: Interrupt ID %u not handled!\n", __func__, irq_num);
+
+	return 0;
+}
+
+int cactus_fiq_handler(void)
+{
+	ffa_int_id_t fiq_num;
+
+	fiq_num = spm_interrupt_get();
+
+	ERROR("%s: Interrupt ID %u not handled!\n", __func__, fiq_num);
+
+	return 0;
+}

islet-project/assets

Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>

spm/cactus/cactus_interrupt.c
@@ -1,0 +1,32 @@
+/*
+ * Copyright (c) 2021, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <debug.h>
+
+#include <ffa_helpers.h>
+#include <sp_helpers.h>
+
+int cactus_irq_handler(void)
+{
+	uint32_t irq_num;
+
+	irq_num = spm_interrupt_get();
+
+	ERROR("%s: Interrupt ID %u not handled!\n", __func__, irq_num);
+
+	return 0;
+}
+
+int cactus_fiq_handler(void)
+{
+	uint32_t fiq_num;
+
+	fiq_num = spm_interrupt_get();
+
+	ERROR("%s: Interrupt ID %u not handled!\n", __func__, fiq_num);
+
+	return 0;
+}