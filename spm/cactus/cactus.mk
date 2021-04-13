duzailian/tf-a-tests

spmc: hypervisor calls moved to a separate module
Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
Change-Id: I285ffc684aa780246468355add9d03c1b5b8fefb

spm/cactus/cactus.mk
@@ -38,6 +38,7 @@ CACTUS_SOURCES	:=					\
 	$(addprefix spm/common/,			\
 		aarch64/sp_arch_helpers.S		\
 		sp_helpers.c				\
+		spmc.c					\
 	)						\
 	$(addprefix spm/cactus/cactus_tests/,		\
 		cactus_message_loop.c			\

spm/cactus/cactus_debug.c
@@ -6,7 +6,7 @@
 
 #include <drivers/arm/pl011.h>
 #include <drivers/console.h>
-#include <sp_helpers.h>
+#include <spmc.h>
 
 #include "cactus.h"
 

spm/cactus/cactus_main.c
@@ -20,6 +20,7 @@
 #include <plat/common/platform.h>
 #include <platform_def.h>
 #include <sp_helpers.h>
+#include <spmc.h>
 #include <std_svc.h>
 
 #include "cactus_def.h"

spm/cactus/cactus_tests/cactus_test_ffa.c
@@ -11,6 +11,7 @@
 #include <cactus_platform_def.h>
 #include <ffa_endpoints.h>
 #include <sp_helpers.h>
+#include <spmc.h>
 #include <spm_common.h>
 
 #include <lib/libc/string.h>

islet-project/assets

spm/cactus/cactus.mk
@@ -38,6 +38,7 @@ CACTUS_SOURCES	:=					\
 	$(addprefix spm/common/,			\
 		aarch64/sp_arch_helpers.S		\
 		sp_helpers.c				\
+		spm_helpers.c					\
 	)						\
 	$(addprefix spm/cactus/cactus_tests/,		\
 		cactus_message_loop.c			\

spm/cactus/cactus_debug.c
@@ -6,7 +6,7 @@
 
 #include <drivers/arm/pl011.h>
 #include <drivers/console.h>
-#include <sp_helpers.h>
+#include <spm_helpers.h>
 
 #include "cactus.h"
 

spm/cactus/cactus_main.c
@@ -20,6 +20,7 @@
 #include <plat/common/platform.h>
 #include <platform_def.h>
 #include <sp_helpers.h>
+#include <spm_helpers.h>
 #include <std_svc.h>
 
 #include "cactus_def.h"

spm/cactus/cactus_tests/cactus_test_ffa.c
@@ -11,6 +11,7 @@
 #include <cactus_platform_def.h>
 #include <ffa_endpoints.h>
 #include <sp_helpers.h>
+#include <spm_helpers.h>
 #include <spm_common.h>
 
 #include <lib/libc/string.h>