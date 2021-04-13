duzailian/tf-a-tests

Cactus: add handler from managed exit FIQ interrupt
Change-Id: I6d4a8c39287dc5102e5fca7af1a4c92bb2d8fbc7
Signed-off-by: Manish Pandey <manish.pandey2@arm.com>

spm/cactus/cactus_interrupt.c
@@ -9,6 +9,19 @@
 #include <ffa_helpers.h>
 #include <sp_helpers.h>
 
+#include "cactus_test_cmds.h"
+
+extern ffa_vm_id_t ffa_id;
+
+static void managed_exit_handler(void)
+{
+	/*
+	 * Real SP will save its context here.
+	 * Send interrupt ID for acknowledgement
+	 */
+	cactus_response(ffa_id, HYP_ID, MANAGED_EXIT_INTERRUPT_ID);
+}
+
 int cactus_irq_handler(void)
 {
 	ffa_int_id_t irq_num;
@@ -26,7 +39,11 @@ int cactus_fiq_handler(void)
 
 	fiq_num = spm_interrupt_get(INTERRUPT_TYPE_FIQ);
 
-	ERROR("%s: Interrupt ID %u not handled!\n", __func__, fiq_num);
+	if (fiq_num == MANAGED_EXIT_INTERRUPT_ID) {
+		managed_exit_handler();
+	} else {
+		ERROR("%s: Interrupt ID %u not handled!\n", __func__, fiq_num);
+	}
 
 	return 0;
 }

islet-project/assets

Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>

spm/cactus/cactus_interrupt.c
@@ -10,6 +10,20 @@
 #include <sp_helpers.h>
 #include <spm_helpers.h>
 
+#include "cactus_test_cmds.h"
+#include "spm_common.h"
+
+extern ffa_vm_id_t g_ffa_id;
+
+static void managed_exit_handler(void)
+{
+	/*
+	 * Real SP will save its context here.
+	 * Send interrupt ID for acknowledgement
+	 */
+	cactus_response(g_ffa_id, HYP_ID, MANAGED_EXIT_INTERRUPT_ID);
+}
+
 int cactus_irq_handler(void)
 {
 	uint32_t irq_num;
@@ -27,7 +41,11 @@ int cactus_fiq_handler(void)
 
 	fiq_num = spm_interrupt_get();
 
-	ERROR("%s: Interrupt ID %u not handled!\n", __func__, fiq_num);
+	if (fiq_num == MANAGED_EXIT_INTERRUPT_ID) {
+		managed_exit_handler();
+	} else {
+		ERROR("%s: Interrupt ID %u not handled!\n", __func__, fiq_num);
+	}
 
 	return 0;
 }