9682b7739c9dcd7d4baa2a638127ae3994709a58
Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
2021-05-24 11:08:40 +0100

    plat: tc0: set PLAT_CACTUS_CORE_COUNT, disable SMMUv3 tests
    Define platform specific SMMUv3 test driver macros. These macros will
    be set to proper values once SMMUv3 is enabled for TC platform.
    
    Disables SMMUv3 tests.
    Enables PSCI NODE_HW_STATE API test.
    
    Change-Id: I1bd0f362521d892d4b33a2188bf8c5065c45d6f0
    Signed-off-by: Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>

--- spm/cactus/plat/arm/tc0/include/cactus_platform_def.h
+++ spm/cactus/plat/arm/tc0/include/cactus_platform_def.h
@@ -16,9 +16,20 @@
 #define CACTUS_PL011_UART_CLK_IN_HZ	PL011_UART1_CLK_IN_HZ
 
 #define PLAT_CACTUS_RX_BASE		ULL(0xfe300000)
+#define PLAT_CACTUS_CORE_COUNT		(8)
 
 #define CACTUS_PRIMARY_EC_COUNT		(8U)
 #define CACTUS_SECONDARY_EC_COUNT	(8U)
 #define CACTUS_TERTIARY_EC_COUNT	(1U)
+
+/* Scratch memory used for SMMUv3 driver testing purposes in Cactus SP */
+/* SMMUv3 tests are disabled for TC platform */
+#define PLAT_CACTUS_MEMCPY_BASE		ULL(0xfe400000)
+#define PLAT_CACTUS_MEMCPY_RANGE	ULL(0x8000)
+
+/* Base address of user and PRIV frames in SMMUv3TestEngine */
+/* SMMUv3 tests are disabled for TC platform */
+#define USR_BASE_FRAME			ULL(0x0)
+#define PRIV_BASE_FRAME			ULL(0x0)
 
 #endif /* CACTUS_PLATFORM_DEF_H */
