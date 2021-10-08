duzailian/tf-a-tests

feat(ff-a): notifications bitmap create and destroy ABIs
Defined wrappers for FF-A calls:
- FFA_NOTIFICATION_BITMAP_CREATE;
- FFA_NOTIFICATION_BITMAP_DESTROY;

Signed-off-by: J-Alves <joao.alves@arm.com>
Change-Id: Ia3dfbfa083807d696f937d677516d80223f04f58

include/runtime_services/ffa_helpers.h
@@ -415,6 +415,9 @@ smc_ret_values ffa_mem_retrieve_req(uint32_t descriptor_length,
 			            uint32_t fragment_length);
 smc_ret_values ffa_mem_relinquish(void);
 smc_ret_values ffa_mem_reclaim(uint64_t handle, uint32_t flags);
+smc_ret_values ffa_notification_bitmap_create(ffa_id_t vm_id,
+					      uint32_t vcpu_count);
+smc_ret_values ffa_notification_bitmap_destroy(ffa_id_t vm_id);
 
 #endif /* __ASSEMBLY__ */
 

tftf/tests/runtime_services/secure_service/ffa_helpers.c
@@ -478,3 +478,39 @@ smc_ret_values ffa_mem_reclaim(uint64_t handle, uint32_t flags)
 
 	return tftf_smc(&args);
 }
+
+/** Create Notifications Bitmap for the given VM */
+smc_ret_values ffa_notification_bitmap_create(ffa_id_t vm_id,
+					      uint32_t vcpu_count)
+{
+	smc_args args = {
+		.fid = FFA_NOTIFICATION_BITMAP_CREATE,
+		.arg1 = vm_id,
+		.arg2 = vcpu_count,
+		.arg3 = FFA_PARAM_MBZ,
+		.arg4 = FFA_PARAM_MBZ,
+		.arg5 = FFA_PARAM_MBZ,
+		.arg6 = FFA_PARAM_MBZ,
+		.arg7 = FFA_PARAM_MBZ
+	};
+
+	return tftf_smc(&args);
+}
+
+/** Destroy Notifications Bitmap for the given VM */
+smc_ret_values ffa_notification_bitmap_destroy(ffa_id_t vm_id)
+{
+	smc_args args = {
+		.fid = FFA_NOTIFICATION_BITMAP_DESTROY,
+		.arg1 = vm_id,
+		.arg2 = FFA_PARAM_MBZ,
+		.arg3 = FFA_PARAM_MBZ,
+		.arg4 = FFA_PARAM_MBZ,
+		.arg5 = FFA_PARAM_MBZ,
+		.arg6 = FFA_PARAM_MBZ,
+		.arg7 = FFA_PARAM_MBZ
+	};
+
+	return tftf_smc(&args);
+}
+

islet-project/assets

include/runtime_services/ffa_helpers.h
@@ -425,6 +425,9 @@ smc_ret_values ffa_mem_retrieve_req(uint32_t descriptor_length,
 			            uint32_t fragment_length);
 smc_ret_values ffa_mem_relinquish(void);
 smc_ret_values ffa_mem_reclaim(uint64_t handle, uint32_t flags);
+smc_ret_values ffa_notification_bitmap_create(ffa_id_t vm_id,
+					      ffa_vcpu_count_t vcpu_count);
+smc_ret_values ffa_notification_bitmap_destroy(ffa_id_t vm_id);
 
 #endif /* __ASSEMBLY__ */
 

tftf/tests/runtime_services/secure_service/ffa_helpers.c
@@ -499,3 +499,39 @@ smc_ret_values ffa_mem_reclaim(uint64_t handle, uint32_t flags)
 
 	return tftf_smc(&args);
 }
+
+/** Create Notifications Bitmap for the given VM */
+smc_ret_values ffa_notification_bitmap_create(ffa_id_t vm_id,
+					      ffa_vcpu_count_t vcpu_count)
+{
+	smc_args args = {
+		.fid = FFA_NOTIFICATION_BITMAP_CREATE,
+		.arg1 = vm_id,
+		.arg2 = vcpu_count,
+		.arg3 = FFA_PARAM_MBZ,
+		.arg4 = FFA_PARAM_MBZ,
+		.arg5 = FFA_PARAM_MBZ,
+		.arg6 = FFA_PARAM_MBZ,
+		.arg7 = FFA_PARAM_MBZ,
+	};
+
+	return tftf_smc(&args);
+}
+
+/** Destroy Notifications Bitmap for the given VM */
+smc_ret_values ffa_notification_bitmap_destroy(ffa_id_t vm_id)
+{
+	smc_args args = {
+		.fid = FFA_NOTIFICATION_BITMAP_DESTROY,
+		.arg1 = vm_id,
+		.arg2 = FFA_PARAM_MBZ,
+		.arg3 = FFA_PARAM_MBZ,
+		.arg4 = FFA_PARAM_MBZ,
+		.arg5 = FFA_PARAM_MBZ,
+		.arg6 = FFA_PARAM_MBZ,
+		.arg7 = FFA_PARAM_MBZ,
+	};
+
+	return tftf_smc(&args);
+}
+