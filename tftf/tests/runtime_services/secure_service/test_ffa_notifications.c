duzailian/tf-a-tests

test(ff-a): bind and unbind notifications
Added tests to FFA_NOTIFICATION_BIND and FFA_NOTIFICATION_UNBIND calls:
- Cactus is requested to perform bind and unbind operations, using
test commands from previous commits.
- Use both calls in TFTF, to test use from VMs.

Signed-off-by: J-Alves <joao.alves@arm.com>
Change-Id: I970882a8f5de960e661f1c7fba01fc4870d8c40f

tftf/tests/runtime_services/secure_service/test_ffa_notifications.c
@@ -15,6 +15,16 @@
 #include <test_helpers.h>
 #include <spm_common.h>
 
+static const struct ffa_uuid expected_sp_uuids[] = {
+		{PRIMARY_UUID}, {SECONDARY_UUID}, {TERTIARY_UUID}
+};
+
+static uint64_t g_notifications = FFA_NOTIFICATION(0)  |
+				  FFA_NOTIFICATION(1)  |
+				  FFA_NOTIFICATION(30) |
+				  FFA_NOTIFICATION(50) |
+				  FFA_NOTIFICATION(63);
+
 /**
  * Helper to create bitmap for NWd VMs.
  */
@@ -102,6 +112,236 @@ test_result_t test_ffa_notifications_create_after_create(void)
 
 	/* Destroy to not affect other tests */
 	if (!notifications_bitmap_destroy(vm_id)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Helper function to test FFA_NOTIFICATION_BIND interface.
+ * Receives all arguments to use 'cactus_notification_bind_send_cmd', and
+ * expected response for the test command.
+ *
+ * Returns:
+ * - 'true' if response was obtained and it was as expected;
+ * - 'false' if there was an error with use of FFA_MSG_SEND_DIRECT_REQ, or
+ * the obtained response was not as expected.
+ */
+static bool request_notification_bind(
+	ffa_id_t cmd_dest, ffa_id_t receiver, ffa_id_t sender,
+	uint64_t notifications, uint32_t flags, uint32_t expected_resp,
+	uint32_t error_code)
+{
+	smc_ret_values ret;
+
+	VERBOSE("TFTF requesting SP to bind notifications!\n");
+
+	ret = cactus_notification_bind_send_cmd(HYP_ID, cmd_dest, receiver,
+						sender, notifications, flags);
+
+	return is_expected_cactus_response(ret, expected_resp, error_code);
+}
+
+/**
+ * Helper function to test FFA_NOTIFICATION_UNBIND interface.
+ * Receives all arguments to use 'cactus_notification_unbind_send_cmd', and
+ * expected response for the test command.
+ *
+ * Returns:
+ * - 'true' if response was obtained and it was as expected;
+ * - 'false' if there was an error with use of FFA_MSG_SEND_DIRECT_REQ, or
+ * the obtained response was not as expected.
+ */
+static bool request_notification_unbind(
+	ffa_id_t cmd_dest, ffa_id_t receiver, ffa_id_t sender,
+	uint64_t notifications, uint32_t expected_resp, uint32_t error_code)
+{
+	smc_ret_values ret;
+
+	VERBOSE("TFTF requesting SP to unbind notifications!\n");
+
+	ret = cactus_notification_unbind_send_cmd(HYP_ID, cmd_dest, receiver,
+						  sender, notifications);
+
+	return is_expected_cactus_response(ret, expected_resp, error_code);
+}
+
+/**
+ * Optimistic test to calls from SPs of the bind and unbind interfaces.
+ * This test issues a request via direct messaging to the SP, which executes
+ * the test and responds with the result to the call.
+ */
+test_result_t test_ffa_notifications_sp_bind_unbind(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 0, expected_sp_uuids);
+
+	/** First bind... */
+	if (!request_notification_bind(SP_ID(1), SP_ID(1), SP_ID(2),
+				       g_notifications, 0, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!request_notification_bind(SP_ID(1), SP_ID(1), 1,
+				       g_notifications, 0, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/** ... then unbind using the same arguments. */
+	if (!request_notification_unbind(SP_ID(1), SP_ID(1), SP_ID(2),
+					 g_notifications, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!request_notification_unbind(SP_ID(1), SP_ID(1), 1,
+					 g_notifications, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Test successful attempt of doing bind and unbind of the same set of
+ * notifications.
+ */
+test_result_t test_ffa_notifications_vm_bind_unbind(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 0, expected_sp_uuids);
+	const ffa_id_t vm_id = 1;
+	smc_ret_values ret;
+
+	if (!notifications_bitmap_create(vm_id, 1)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	ret = ffa_notification_bind(SP_ID(2), vm_id, 0, g_notifications);
+
+	if (!is_expected_ffa_return(ret, FFA_SUCCESS_SMC32)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	ret = ffa_notification_unbind(SP_ID(2), vm_id, g_notifications);
+
+	if (!is_expected_ffa_return(ret, FFA_SUCCESS_SMC32)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!notifications_bitmap_destroy(vm_id)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Test expected failure of using a NS FF-A ID for the sender.
+ */
+test_result_t test_ffa_notifications_vm_bind_vm(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 0, expected_sp_uuids);
+	const ffa_id_t vm_id = 1;
+	const ffa_id_t sender_id = 2;
+	smc_ret_values ret;
+
+	if (!notifications_bitmap_create(vm_id, 1)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	ret = ffa_notification_bind(sender_id, vm_id, 0, g_notifications);
+
+	if (!is_expected_ffa_error(ret, FFA_ERROR_INVALID_PARAMETER)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!notifications_bitmap_destroy(vm_id)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Test failure of both bind and unbind in case at least one notification is
+ * already bound to another FF-A endpoint.
+ * Expect error code FFA_ERROR_DENIED.
+ */
+test_result_t test_ffa_notifications_already_bound(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 0, expected_sp_uuids);
+
+	/** Bind first to test */
+	if (!request_notification_bind(SP_ID(1), SP_ID(1), SP_ID(2),
+				       g_notifications, 0, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/** Attempt to bind notifications bound in above request. */
+	if (!request_notification_bind(SP_ID(1), SP_ID(1), SP_ID(3),
+				       g_notifications, 0, CACTUS_ERROR,
+				       FFA_ERROR_DENIED)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/** Attempt to unbind notifications bound in initial request. */
+	if (!request_notification_unbind(SP_ID(1), SP_ID(1), SP_ID(3),
+					 g_notifications, CACTUS_ERROR,
+					 FFA_ERROR_DENIED)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/** Reset the state the SP's notifications state. */
+	if (!request_notification_unbind(SP_ID(1), SP_ID(1), SP_ID(2),
+					 g_notifications, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Try to bind/unbind notifications spoofing the identity of the receiver.
+ * Commands will be sent to SP_ID(1), which will use SP_ID(3) as the receiver.
+ * Expect error code FFA_ERROR_INVALID_PARAMETER.
+ */
+test_result_t test_ffa_notifications_bind_unbind_spoofing(void)
+{
+	uint64_t notifications = FFA_NOTIFICATION(8);
+
+	CHECK_SPMC_TESTING_SETUP(1, 0, expected_sp_uuids);
+
+	if (!request_notification_bind(SP_ID(1), SP_ID(3), SP_ID(2),
+				       notifications, 0, CACTUS_ERROR,
+				       FFA_ERROR_INVALID_PARAMETER)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!request_notification_unbind(SP_ID(1), SP_ID(3), SP_ID(2),
+					 notifications, CACTUS_ERROR,
+					 FFA_ERROR_INVALID_PARAMETER)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Call FFA_NOTIFICATION_BIND with notifications bitmap zeroed.
+ * Expecting error code FFA_ERROR_INVALID_PARAMETER.
+ */
+test_result_t test_ffa_notifications_bind_unbind_zeroed(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 0, expected_sp_uuids);
+
+	if (!request_notification_bind(SP_ID(1), SP_ID(1), SP_ID(2),
+				       0, 0, CACTUS_ERROR,
+				       FFA_ERROR_INVALID_PARAMETER)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!request_notification_unbind(SP_ID(1), SP_ID(1), SP_ID(2),
+					 0, CACTUS_ERROR,
+					 FFA_ERROR_INVALID_PARAMETER)) {
 		return TEST_RESULT_FAIL;
 	}
 

islet-project/assets

tftf/tests/runtime_services/secure_service/test_ffa_notifications.c
@@ -15,6 +15,16 @@
 #include <spm_common.h>
 #include <test_helpers.h>
 
+static const struct ffa_uuid expected_sp_uuids[] = {
+		{PRIMARY_UUID}, {SECONDARY_UUID}, {TERTIARY_UUID}
+};
+
+static ffa_notification_bitmap_t g_notifications = FFA_NOTIFICATION(0)  |
+						   FFA_NOTIFICATION(1)  |
+						   FFA_NOTIFICATION(30) |
+						   FFA_NOTIFICATION(50) |
+						   FFA_NOTIFICATION(63);
+
 /**
  * Helper to create bitmap for NWd VMs.
  */
@@ -115,6 +125,238 @@ test_result_t test_ffa_notifications_create_after_create(void)
 
 	/* Destroy to not affect other tests */
 	if (!notifications_bitmap_destroy(vm_id)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Helper function to test FFA_NOTIFICATION_BIND interface.
+ * Receives all arguments to use 'cactus_notification_bind_send_cmd', and
+ * expected response for the test command.
+ *
+ * Returns:
+ * - 'true' if response was obtained and it was as expected;
+ * - 'false' if there was an error with use of FFA_MSG_SEND_DIRECT_REQ, or
+ * the obtained response was not as expected.
+ */
+static bool request_notification_bind(
+	ffa_id_t cmd_dest, ffa_id_t receiver, ffa_id_t sender,
+	ffa_notification_bitmap_t notifications, uint32_t flags,
+	uint32_t expected_resp, uint32_t error_code)
+{
+	smc_ret_values ret;
+
+	VERBOSE("TFTF requesting SP to bind notifications!\n");
+
+	ret = cactus_notification_bind_send_cmd(HYP_ID, cmd_dest, receiver,
+						sender, notifications, flags);
+
+	return is_expected_cactus_response(ret, expected_resp, error_code);
+}
+
+/**
+ * Helper function to test FFA_NOTIFICATION_UNBIND interface.
+ * Receives all arguments to use 'cactus_notification_unbind_send_cmd', and
+ * expected response for the test command.
+ *
+ * Returns:
+ * - 'true' if response was obtained and it was as expected;
+ * - 'false' if there was an error with use of FFA_MSG_SEND_DIRECT_REQ, or
+ * the obtained response was not as expected.
+ */
+static bool request_notification_unbind(
+	ffa_id_t cmd_dest, ffa_id_t receiver, ffa_id_t sender,
+	ffa_notification_bitmap_t notifications, uint32_t expected_resp,
+	uint32_t error_code)
+{
+	smc_ret_values ret;
+
+	VERBOSE("TFTF requesting SP to unbind notifications!\n");
+
+	ret = cactus_notification_unbind_send_cmd(HYP_ID, cmd_dest, receiver,
+						  sender, notifications);
+
+	return is_expected_cactus_response(ret, expected_resp, error_code);
+}
+
+/**
+ * Test calls from SPs to the bind and unbind interfaces, expecting success
+ * returns.
+ * This test issues a request via direct messaging to the SP, which executes
+ * the test and responds with the result of the call.
+ */
+test_result_t test_ffa_notifications_sp_bind_unbind(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 1, expected_sp_uuids);
+
+	/** First bind... */
+	if (!request_notification_bind(SP_ID(1), SP_ID(1), SP_ID(2),
+				       g_notifications, 0, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!request_notification_bind(SP_ID(1), SP_ID(1), 1,
+				       g_notifications, 0, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/** ... then unbind using the same arguments. */
+	if (!request_notification_unbind(SP_ID(1), SP_ID(1), SP_ID(2),
+					 g_notifications, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!request_notification_unbind(SP_ID(1), SP_ID(1), 1,
+					 g_notifications, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Test successful attempt of doing bind and unbind of the same set of
+ * notifications.
+ */
+test_result_t test_ffa_notifications_vm_bind_unbind(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 1, expected_sp_uuids);
+	const ffa_id_t vm_id = 1;
+	smc_ret_values ret;
+
+	if (!notifications_bitmap_create(vm_id, 1)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	ret = ffa_notification_bind(SP_ID(2), vm_id, 0, g_notifications);
+
+	if (!is_expected_ffa_return(ret, FFA_SUCCESS_SMC32)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	ret = ffa_notification_unbind(SP_ID(2), vm_id, g_notifications);
+
+	if (!is_expected_ffa_return(ret, FFA_SUCCESS_SMC32)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!notifications_bitmap_destroy(vm_id)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Test expected failure of using a NS FF-A ID for the sender.
+ */
+test_result_t test_ffa_notifications_vm_bind_vm(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 1, expected_sp_uuids);
+	const ffa_id_t vm_id = 1;
+	const ffa_id_t sender_id = 2;
+	smc_ret_values ret;
+
+	if (!notifications_bitmap_create(vm_id, 1)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	ret = ffa_notification_bind(sender_id, vm_id, 0, g_notifications);
+
+	if (!is_expected_ffa_error(ret, FFA_ERROR_INVALID_PARAMETER)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!notifications_bitmap_destroy(vm_id)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Test failure of both bind and unbind in case at least one notification is
+ * already bound to another FF-A endpoint.
+ * Expect error code FFA_ERROR_DENIED.
+ */
+test_result_t test_ffa_notifications_already_bound(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 1, expected_sp_uuids);
+
+	/** Bind first to test */
+	if (!request_notification_bind(SP_ID(1), SP_ID(1), SP_ID(2),
+				       g_notifications, 0, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/** Attempt to bind notifications bound in above request. */
+	if (!request_notification_bind(SP_ID(1), SP_ID(1), SP_ID(3),
+				       g_notifications, 0, CACTUS_ERROR,
+				       FFA_ERROR_DENIED)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/** Attempt to unbind notifications bound in initial request. */
+	if (!request_notification_unbind(SP_ID(1), SP_ID(1), SP_ID(3),
+					 g_notifications, CACTUS_ERROR,
+					 FFA_ERROR_DENIED)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/** Reset the state the SP's notifications state. */
+	if (!request_notification_unbind(SP_ID(1), SP_ID(1), SP_ID(2),
+					 g_notifications, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Try to bind/unbind notifications spoofing the identity of the receiver.
+ * Commands will be sent to SP_ID(1), which will use SP_ID(3) as the receiver.
+ * Expect error code FFA_ERROR_INVALID_PARAMETER.
+ */
+test_result_t test_ffa_notifications_bind_unbind_spoofing(void)
+{
+	ffa_notification_bitmap_t notifications = FFA_NOTIFICATION(8);
+
+	CHECK_SPMC_TESTING_SETUP(1, 1, expected_sp_uuids);
+
+	if (!request_notification_bind(SP_ID(1), SP_ID(3), SP_ID(2),
+				       notifications, 0, CACTUS_ERROR,
+				       FFA_ERROR_INVALID_PARAMETER)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!request_notification_unbind(SP_ID(1), SP_ID(3), SP_ID(2),
+					 notifications, CACTUS_ERROR,
+					 FFA_ERROR_INVALID_PARAMETER)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Call FFA_NOTIFICATION_BIND with notifications bitmap zeroed.
+ * Expecting error code FFA_ERROR_INVALID_PARAMETER.
+ */
+test_result_t test_ffa_notifications_bind_unbind_zeroed(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 1, expected_sp_uuids);
+
+	if (!request_notification_bind(SP_ID(1), SP_ID(1), SP_ID(2),
+				       0, 0, CACTUS_ERROR,
+				       FFA_ERROR_INVALID_PARAMETER)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!request_notification_unbind(SP_ID(1), SP_ID(1), SP_ID(2),
+					 0, CACTUS_ERROR,
+					 FFA_ERROR_INVALID_PARAMETER)) {
 		return TEST_RESULT_FAIL;
 	}
 