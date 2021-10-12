duzailian/tf-a-tests

feat(ff-a): notifications bind and unbind ABIs
Defined wrappers for FF-A calls:
- FFA_NOTIFICATION_BIND;
- FFA_NOTIFICATION_UNBIND;

Signed-off-by: J-Alves <joao.alves@arm.com>
Change-Id: I556faa2ef457f11e4469ca61b98ee4c06915fd01

tftf/tests/runtime_services/secure_service/ffa_helpers.c
@@ -491,7 +491,7 @@ smc_ret_values ffa_notification_bitmap_create(ffa_id_t vm_id,
 		.arg4 = FFA_PARAM_MBZ,
 		.arg5 = FFA_PARAM_MBZ,
 		.arg6 = FFA_PARAM_MBZ,
-		.arg7 = FFA_PARAM_MBZ
+		.arg7 = FFA_PARAM_MBZ,
 	};
 
 	return tftf_smc(&args);
@@ -508,9 +508,46 @@ smc_ret_values ffa_notification_bitmap_destroy(ffa_id_t vm_id)
 		.arg4 = FFA_PARAM_MBZ,
 		.arg5 = FFA_PARAM_MBZ,
 		.arg6 = FFA_PARAM_MBZ,
-		.arg7 = FFA_PARAM_MBZ
+		.arg7 = FFA_PARAM_MBZ,
 	};
 
 	return tftf_smc(&args);
 }
 
+/** Bind VM to all the notifications in the bitmap */
+smc_ret_values ffa_notification_bind(ffa_id_t sender, ffa_id_t receiver,
+				     uint32_t flags,
+				     uint64_t bitmap)
+{
+	smc_args args = {
+		.fid = FFA_NOTIFICATION_BIND,
+		.arg1 = (sender << 16) | (receiver),
+		.arg2 = flags,
+		.arg3 = (uint32_t)(bitmap),
+		.arg4 = (uint32_t)(bitmap >> 32),
+		.arg5 = FFA_PARAM_MBZ,
+		.arg6 = FFA_PARAM_MBZ,
+		.arg7 = FFA_PARAM_MBZ,
+	};
+
+	return tftf_smc(&args);
+}
+
+/** Unbind previously bound VM from notifications in bitmap */
+smc_ret_values ffa_notification_unbind(ffa_id_t sender,
+				       ffa_id_t receiver,
+				       uint64_t bitmap)
+{
+	smc_args args = {
+		.fid = FFA_NOTIFICATION_UNBIND,
+		.arg1 = (sender << 16) | (receiver),
+		.arg2 = FFA_PARAM_MBZ,
+		.arg3 = (uint32_t)(bitmap),
+		.arg4 = (uint32_t)(bitmap >> 32),
+		.arg5 = FFA_PARAM_MBZ,
+		.arg6 = FFA_PARAM_MBZ,
+		.arg7 = FFA_PARAM_MBZ
+	};
+
+	return tftf_smc(&args);
+}

islet-project/assets

tftf/tests/runtime_services/secure_service/ffa_helpers.c
@@ -535,3 +535,40 @@ smc_ret_values ffa_notification_bitmap_destroy(ffa_id_t vm_id)
 	return tftf_smc(&args);
 }
 
+/** Bind VM to all the notifications in the bitmap */
+smc_ret_values ffa_notification_bind(ffa_id_t sender, ffa_id_t receiver,
+				     uint32_t flags,
+				     ffa_notification_bitmap_t bitmap)
+{
+	smc_args args = {
+		.fid = FFA_NOTIFICATION_BIND,
+		.arg1 = (sender << 16) | (receiver),
+		.arg2 = flags,
+		.arg3 = (uint32_t)(bitmap & 0xFFFFFFFFU),
+		.arg4 = (uint32_t)(bitmap >> 32),
+		.arg5 = FFA_PARAM_MBZ,
+		.arg6 = FFA_PARAM_MBZ,
+		.arg7 = FFA_PARAM_MBZ,
+	};
+
+	return tftf_smc(&args);
+}
+
+/** Unbind previously bound VM from notifications in bitmap */
+smc_ret_values ffa_notification_unbind(ffa_id_t sender,
+				       ffa_id_t receiver,
+				       ffa_notification_bitmap_t bitmap)
+{
+	smc_args args = {
+		.fid = FFA_NOTIFICATION_UNBIND,
+		.arg1 = (sender << 16) | (receiver),
+		.arg2 = FFA_PARAM_MBZ,
+		.arg3 = (uint32_t)(bitmap),
+		.arg4 = (uint32_t)(bitmap >> 32),
+		.arg5 = FFA_PARAM_MBZ,
+		.arg6 = FFA_PARAM_MBZ,
+		.arg7 = FFA_PARAM_MBZ,
+	};
+
+	return tftf_smc(&args);
+}