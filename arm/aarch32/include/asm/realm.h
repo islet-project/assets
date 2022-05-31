ea00457725aa3afbcf9fa859dff1bc1a09b46ce0
Alexandru Elisei <alexandru.elisei@arm.com>
2022-05-31 16:15:36 +0100

    arm64: Create Realm Descriptor
    Create the Realm Descriptor using the measurement algorithm set
    with --measurement-algo.
    
    Signed-off-by: Alexandru Elisei <alexandru.elisei@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>

---
+++ arm/aarch32/include/asm/realm.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __ASM_REALM_H
+#define __ASM_REALM_H
+
+#include "kvm/kvm.h"
+
+static inline void kvm_arm_realm_create_realm_descriptor(struct kvm *kvm) {}
+
+#endif /* ! __ASM_REALM_H */
