cf0d92396556457eb8a059af3f43eb8ff85acc5d
Suzuki K Poulose <suzuki.poulose@arm.com>
2022-10-04 12:32:34 +0100

    arm64: Add configuration step for Realms
    Realm must be configured before it is created. Add the
    step to specify the parameters for the Realm.
    
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>

--- arm/aarch64/realm.c
+++ arm/aarch64/realm.c
@@ -2,6 +2,29 @@
 
 #include "asm/realm.h"
 
+
+static void realm_configure_hash_algo(struct kvm *kvm)
+{
+	struct kvm_cap_arm_rme_config_item hash_algo_cfg = {
+		.cfg	= KVM_CAP_ARM_RME_CFG_HASH_ALGO,
+		.hash_algo = kvm->arch.measurement_algo,
+	};
+
+	struct kvm_enable_cap rme_config = {
+		.cap = KVM_CAP_ARM_RME,
+		.args[0] = KVM_CAP_ARM_RME_CONFIG_REALM,
+		.args[1] = (u64)&hash_algo_cfg,
+	};
+
+	if (ioctl(kvm->vm_fd, KVM_ENABLE_CAP, &rme_config) < 0)
+		die_perror("KVM_CAP_RME(KVM_CAP_ARM_RME_CONFIG_REALM) hash_algo");
+}
+
+static void realm_configure_parameters(struct kvm *kvm)
+{
+	realm_configure_hash_algo(kvm);
+}
+
 void kvm_arm_realm_create_realm_descriptor(struct kvm *kvm)
 {
 	struct kvm_enable_cap rme_create_rd = {
@@ -9,6 +32,7 @@ void kvm_arm_realm_create_realm_descriptor(struct kvm *kvm)
 		.args[0] = KVM_CAP_ARM_RME_CREATE_RD,
 	};
 
+	realm_configure_parameters(kvm);
 	if (ioctl(kvm->vm_fd, KVM_ENABLE_CAP, &rme_create_rd) < 0)
 		die_perror("KVM_CAP_RME(KVM_CAP_ARM_RME_CREATE_RD)");
 }
