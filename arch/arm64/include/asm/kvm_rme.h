0dce9dafeee0460bb750c1e0da09871e090b69e7
Suzuki K Poulose <suzuki.poulose@arm.com>
2022-10-10 23:53:24 +0100

    arm64: rme: Allow checking SVE on VM instance
    Given we have different types of VMs supported, check the
    support for SVE for the given instance of the VM to accurately
    report the status.
    
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Steven Price <steven.price@arm.com>

--- arch/arm64/include/asm/kvm_rme.h
+++ arch/arm64/include/asm/kvm_rme.h
@@ -91,6 +91,8 @@ u32 kvm_realm_ipa_limit(void);
 u32 kvm_realm_get_num_brps(void);
 u32 kvm_realm_get_num_wrps(void);
 
+bool kvm_rme_supports_sve(void);
+
 int kvm_realm_enable_cap(struct kvm *kvm, struct kvm_enable_cap *cap);
 int kvm_init_realm_vm(struct kvm *kvm);
 void kvm_destroy_realm(struct kvm *kvm);
