cce5fa50b171cb2e9b943ab97c0210616b2a7268
Suzuki K Poulose <suzuki.poulose@arm.com>
2022-10-12 10:14:31 +0100

    arm64: Add support for Realm Personalisation Value
    Add option to specify Realm personalisation value
    
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>

--- arm/aarch64/kvm.c
+++ arm/aarch64/kvm.c
@@ -55,6 +55,8 @@ static void validate_realm_cfg(struct kvm *kvm)
 	if (!kvm__is_realm(kvm)) {
 		if (kvm->cfg.arch.measurement_algo)
 			die("--measurement-algo valid only with --realm");
+		if (kvm->cfg.arch.realm_pv)
+			die("--realm-pv valid only with --realm");
 		return;
 	}
 
@@ -71,6 +73,11 @@ static void validate_realm_cfg(struct kvm *kvm)
 	} else {
 		pr_debug("Realm Hash algorithm: Using default SHA256\n");
 		kvm->arch.measurement_algo = KVM_CAP_ARM_RME_MEASUREMENT_ALGO_SHA256;
+	}
+
+	if (kvm->cfg.arch.realm_pv) {
+		if (strlen(kvm->cfg.arch.realm_pv) > KVM_CAP_ARM_RME_RPV_SIZE)
+			die("Invalid size for Realm Personalization Value\n");
 	}
 
 	die("Realms not supported");
