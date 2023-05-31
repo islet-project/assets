f68384eeb8f3359b42b4e88f4b671d11f168b4aa
Jinbum Park <jinb.park@samsung.com>
2023-05-31 16:34:21 +0900

    update prebuilt examples to support interaction with web werver in ARM CCA
    Signed-off-by: Jinbum Park <jinb.park@samsung.com>

--- prebuilt/examples/confidential-ml/certifier-service/run.sh
+++ prebuilt/examples/confidential-ml/certifier-service/run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 ARCH=$1
+HOST="0.0.0.0"
+if [ "$2" ]; then
+  HOST=$2
+fi
+
 cp -f ../certifier-data/* .
-./simpleserver.${ARCH} --policyFile=policy.bin --readPolicy=true --host="0.0.0.0"
+./simpleserver.${ARCH} --policyFile=policy.bin --readPolicy=true --host="${HOST}"
