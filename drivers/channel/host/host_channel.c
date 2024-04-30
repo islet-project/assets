cdbe17dc96851435d3eb0cbdb1cdaac55d9b9e0d
Sunwook Eom <speed.eom@samsung.com>
2024-04-30 12:56:00 +0900

    fix: Fix some build errors
    Signed-off-by: Sunwook Eom <speed.eom@samsung.com>

--- drivers/channel/host/host_channel.c
+++ drivers/channel/host/host_channel.c
@@ -2,7 +2,8 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
-#include <string.h>
+#include <linux/string.h>
+#include <linux/printk.h>
 
 #define DRIVER_NAME "host_channel"
 #define PEER_LIST_MAX  128
@@ -15,7 +16,7 @@ static int dev_major_num;
 struct peer {
     int id;      /* This is for identifying peers. It's NOT vmid */
     int eventfd;
-}
+};
 
 /* This is a "private" data structure */
 struct channel_priv {
@@ -27,38 +28,6 @@ struct channel_priv {
 
 // TODO: Need lock
 static struct channel_priv drv_priv;
-
-static const struct file_operations channel_ops = {
-	.owner   = THIS_MODULE,
-	.open	= channel_open,
-	.write   = channel_write,
-	.release = channel_release,
-};
-
-static int channel_open(struct inode *inode, struct file *file)
-{
-	pr_info("CHANNEL: device opened\n");
-	struct peer *peer = kmalloc(sizeof(struct peer), GFP_KERNEL);
-
-    if (!peer) {
-		pr_err("CHANNEL: kmalloc failed");
-        return -ENOMEM;
-    }
-
-    file->private_data = peer;
-
-    return 0;
-}
-
-static int channel_release(struct inode * inode, struct file * filp)
-{
-    pr_info("CHANNEL: channel_release start");
-    if (file->private_data) {
-        kfree(file->private_data);
-        file->private_data = NULL;
-    }
-    return 0;
-}
 
 static int search_peer_idx(int peer_id) {
     for (int i = 0; i < drv_priv.cnt; i++) {
@@ -83,36 +52,71 @@ static bool push_back(struct peer new_peer) {
     return true;
 }
 
-static int channel_write(struct file *file, const char __user *user_buffer,
-                    size_t size, loff_t * offset)
+static int channel_open(struct inode *inode, struct file *file)
+{
+	struct peer *peer;
+	pr_info("CHANNEL: device opened\n");
+
+	peer = kmalloc(sizeof(struct peer), GFP_KERNEL);
+    if (!peer) {
+		pr_err("CHANNEL: kmalloc failed");
+        return -ENOMEM;
+    }
+
+    file->private_data = peer;
+
+    return 0;
+}
+
+static int channel_release(struct inode * inode, struct file * file)
+{
+    pr_info("CHANNEL: channel_release start");
+    if (file->private_data) {
+        kfree(file->private_data);
+        file->private_data = NULL;
+    }
+    return 0;
+}
+
+static ssize_t channel_write(struct file *file, const char __user *user_buffer, size_t size, loff_t * offset)
 {
 	int ret = 0;
-    struct peer *peer= (struct peer*) file->private_data;
-    ssize_t len = min(sizeof(struct peer) - *offset, size);
+    struct peer *peer = (struct peer*) file->private_data;
+    ssize_t len = sizeof(struct peer) - *offset;
 
     if (len <= 0)
         return 0;
 
     /* read data from user buffer to my_data->buffer */
-    if (copy_from_user(peer + *offset, user_buffer, len))
-        return -EFAULT;
+    ret = copy_from_user(peer + *offset, user_buffer, len);
+    if (ret) {
+		return -EFAULT;
+	}
 
-	pr_info("CHANNEL: channel_write done. peer: %d %d %d len: %d",
-			peer->id, peer->sock_fd, peer->eventfd, len);
+	pr_info("CHANNEL: channel_write done. peer: %d %d len: %d\n", peer->id, peer->eventfd, len);
 	// TODO: add peer to the peer list
-	if (drv_priv.id == -1 || drv_priv.eventfd == -1) {
-		drv_priv.id = peer.id;
-		drv_priv.eventfd = peer.eventfd;
-	} else if (is_new_peer(peer.id)) {
-		ret = push_back(peer);
+	if (drv_priv.id < 0 || drv_priv.eventfd < 0) {
+		drv_priv.id = peer->id;
+		drv_priv.eventfd = peer->eventfd;
+	} else if (is_new_peer(peer->id)) {
+		ret = push_back(*peer);
 		if (ret) {
-			pr_err("failed to push_back a new peer. id: %d eventfd: %d", peer.id, peer.eventfd);
+			pr_err("failed to push_back a new peer. id: %d, eventfd: %d", peer->id, peer->eventfd);
 		}
+	} else {
+		pr_err("The peer already exists: id: %d, eventfd: %d", peer->id, peer->eventfd);
 	}
 
     *offset += len;
     return len;
 }
+
+static const struct file_operations channel_ops = {
+	.owner   = THIS_MODULE,
+	.open	= channel_open,
+	.write   = channel_write,
+	.release = channel_release,
+};
 
 static int __init channel_init(void)
 {
@@ -130,17 +134,10 @@ static int __init channel_init(void)
 	pr_info("CHANNEL: major device numer: %d\n", dev_major_num);
 
 	return 0;
-
-err:
-	unregister_chrdev(dev_major_num, DRIVER_NAME);
-	return ret;
 }
 
 static void __exit channel_exit(void)
 {
-    int minor; 
-    dev_t dev = MKDEV(dev_major_num, MINOR_BASE);
-
 	unregister_chrdev(dev_major_num, DRIVER_NAME);
 }
 
