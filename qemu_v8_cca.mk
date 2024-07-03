4f09178bc8c9c302e23eca7cf05771fe9e4c3b57
Mathieu Poirier <mathieu.poirier@linaro.org>
2024-07-03 11:26:09 -0600

    qemu_v8_cca: Adding buildroot configs for EXT4 support
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- qemu_v8_cca.mk
+++ qemu_v8_cca.mk
@@ -17,6 +17,11 @@ BR2_ROOTFS_OVERLAY = $(ROOT)/build/br-ext/board/qemu/overlay
 BR2_ROOTFS_POST_BUILD_SCRIPT = $(ROOT)/build/br-ext/board/qemu/post-build.sh
 BR2_ROOTFS_POST_SCRIPT_ARGS = "$(QEMU_VIRTFS_AUTOMOUNT) $(QEMU_VIRTFS_MOUNTPOINT) $(QEMU_PSS_AUTOMOUNT)"
 
+BR2_TARGET_ROOTFS_EXT2=y
+BR2_TARGET_ROOTFS_EXT2_4=y
+BR2_TARGET_ROOTFS_EXT2_LABEL="rootfs"
+BR2_TARGET_ROOTFS_EXT2_SIZE="256M"
+
 OPTEE_OS_PLATFORM = vexpress-qemu_armv8a
 
 ########################################################################################
