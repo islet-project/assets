869f0d912c9b2ecd6f8f73cbc229651c9ac1e602
Sunwook Eom <speed.eom@samsung.com>
2024-10-23 14:02:48 +0900

    debug: Add more log to check is_swiotlb_for_alloc(dev)
    Signed-off-by: Sunwook Eom <speed.eom@samsung.com>

--- drivers/virtio/virtio_pci_modern.c
+++ drivers/virtio/virtio_pci_modern.c
@@ -23,7 +23,8 @@
 
 static void *eom_test_alloc(struct device *dev, size_t size,
 		dma_addr_t *dma_handle, gfp_t gfp, unsigned long attrs) {
-	pr_info("%s: start! size: %#llx, gfp: %#llx, attrs: %#llx", __func__, size, gfp, attrs);
+	pr_info("%s: start! size: %#llx, gfp: %#llx, attrs: %#llx, dev->dma_io_tlb_mem->for_alloc: %d",
+			__func__, size, gfp, attrs, dev->dma_io_tlb_mem->for_alloc);
 	dump_stack();
 	return dma_direct_alloc(dev, size, dma_handle, gfp, attrs);
 }
